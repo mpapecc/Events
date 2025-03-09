602012dffbbaa25d531f6150cf59b9d277a68935
Milivoj <Milivoj@DESKTOP-E8EP9NR>
2025-03-09 18:11:14 +0100

    register dependencies, come startup configurations

--- Events.Api/Program.cs
+++ Events.Api/Program.cs
@@ -1,8 +1,22 @@
+using System.Text.Json.Serialization;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Events.Application;
+using Events.Persistance;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(c =>
+    {
+        c.RegisterModule(new PersistanceIocModule(builder.Configuration));
+        c.RegisterModule(new ApplicationIocModule());
+    });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(o => 
+    o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
