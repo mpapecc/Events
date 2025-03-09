af524fc8606c0b312d1ce22a3955bdcb504d8ca5
Milivoj <Milivoj@DESKTOP-E8EP9NR>
2025-03-09 18:04:27 +0100

    Domain project

---
+++ Events.Domain/Entities/Event.cs
@@ -0,0 +1,11 @@
+﻿using Events.Domain.Entities.BaseEntites;
+
+namespace Events.Domain.Entities
+{
+    public class Event : BaseEntityWithChangeTrackingAndRecordStatus
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDateTime { get; set; }
+    }
+}
