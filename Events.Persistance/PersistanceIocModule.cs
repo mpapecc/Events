6a2dea106d50bfc70763b50cb880a9f103495a44
Milivoj <Milivoj@DESKTOP-E8EP9NR>
2025-03-09 18:08:05 +0100

    create EventsDbContext, Repository class, use Autofac as DI framework

---
+++ Events.Persistance/PersistanceIocModule.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Events.Application.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Events.Persistance
+{
+    public class PersistanceIocModule : Module
+    {
+        private readonly IConfiguration configuration;
+
+        public PersistanceIocModule(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
+
+            builder.Register(x =>
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<EventsDbContext>();
+                optionsBuilder.UseNpgsql(configuration.GetConnectionString("Default"));
+                return new EventsDbContext(optionsBuilder.Options);
+            }).InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ Events.Persistance/Repository.cs
@@ -0,0 +1,25 @@
+﻿using Events.Application.Repositories;
+using Events.Domain.Entities.BaseEntites;
+
+namespace Events.Persistance
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly EventsDbContext context;
+
+        public Repository(EventsDbContext context)
+        {
+            this.context = context;
+        }
+
+        public virtual T? GetById(int id)
+        {
+            return this.context.Set<T>().Find(id);
+        }
+
+        public virtual IQueryable<T> Query()
+        {
+            return this.context.Set<T>();
+        }
+    }
+}
