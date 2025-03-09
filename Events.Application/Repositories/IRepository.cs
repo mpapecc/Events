dee06614eb2b827259c6a891a9281c6bf21993e0
Milivoj <Milivoj@DESKTOP-E8EP9NR>
2025-03-09 18:10:44 +0100

    Application project => business logic layer

---
+++ Events.Application/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using Events.Domain.Entities.BaseEntites;
+
+namespace Events.Application.Repositories
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T? GetById(int id);
+        IQueryable<T> Query();
+    }
+}
