d20af78be765482e18e927dd0fbbfa7acf3e971c
Milivoj <Milivoj@DESKTOP-E8EP9NR>
2025-03-09 20:57:13 +0100

    remove ignoring cycles

--- Events.Api/Program.cs
+++ Events.Api/Program.cs
@@ -14,9 +14,6 @@ builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
         c.RegisterModule(new ApplicationIocModule());
     });
 
-builder.Services.AddControllers().AddJsonOptions(o => 
-    o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
